6be59c2b3688844f24768133e44e45e80329271c
Shankaz <shankazcloud@gmail.com>
2025-12-26 18:07:45 +0530

    added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,14 @@
 output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.python_app.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "ECS cluster name"
+  value       = aws_ecs_cluster.python_cluster.name
+}
+
+output "ecs_service_name" {
+  description = "ECS service name"
+  value       = aws_ecs_service.python_service.name
 }
